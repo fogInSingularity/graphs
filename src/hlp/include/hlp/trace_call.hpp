3ec4c6d76733193d895a3d6eb3aa86c2d34eb804
fogInSingularity <vladimirnaumov979@gmail.com>
2025-12-03 23:13:36 +0300

    hello world commit

---
+++ src/hlp/include/hlp/trace_call.hpp
@@ -0,0 +1,19 @@
+#ifndef TRACE_CALLS_HPP_
+#define TRACE_CALLS_HPP_
+
+#include <source_location>
+
+#include <spdlog/spdlog.h>
+
+namespace hlp {
+
+inline void trace_call([[maybe_unused]] std::source_location loc = std::source_location::current()) {
+#if !defined(NDEBUG)
+    // spdlog::debug("Function call: {}, {}:{}", loc.function_name(), loc.file_name(), loc.line());
+    spdlog::debug("Function call: {}", loc.function_name());
+#endif // NDEBUG
+}
+
+} // namespace hlp
+
+#endif // TRACE_CALLS_HPP_
